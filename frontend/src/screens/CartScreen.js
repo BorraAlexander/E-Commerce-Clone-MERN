241effc2f36a15a0faa34f5de3645b9f708f83ef
borra alexander <alexkign488@gmail.com>
2026-05-09 15:46:07 +0530

    updated shipping page redirect

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -27,7 +27,7 @@ const CartScreen = () => {
   };
 
   const checkOutHandler = () => {
-    navigate("/login?redirect=shipping");
+    navigate("/login?redirect=/shipping");
   };
   return (
     <div>
